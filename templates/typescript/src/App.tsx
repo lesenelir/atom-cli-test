10931957dc0bc8a841f89a284b8ff675db6dd1b1
lesenelir <hi@lesenelir.me>
2023-09-26 21:57:01 +0800

    feat: ts template content

---
+++ templates/typescript/src/App.tsx
@@ -0,0 +1,27 @@
+import {useState} from "react"
+
+import styles from './app.module.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  const handleClick = () => {
+    setCount(count => count + 1)
+  }
+
+  return (
+    <div className={styles.box}>
+      <div className={styles.state}>
+        <span>count: {count}</span>
+        <button className={styles.btn} onClick={handleClick}>Click</button>
+      </div>
+
+      <div className={styles.words}>
+        <p>A template package for building React application by own cli tool.</p>
+        <p className={styles.author}> --- Lesenelir</p>
+      </div>
+    </div>
+  )
+}
+
+export default App
