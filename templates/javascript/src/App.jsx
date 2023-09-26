fbc295ebbd577ce8cc0db5f8642b49be909e7e05
lesenelir <hi@lesenelir.me>
2023-09-26 20:39:57 +0800

    chore: change template content

--- templates/javascript/src/App.jsx
+++ templates/javascript/src/App.jsx
@@ -1,8 +1,26 @@
+import {useState} from "react"
+
+import styles from './App.module.css'
+
 function App() {
+  const [count, setCount] = useState(0)
+
+  const handleClick = () => {
+    setCount(count => count + 1)
+  }
+
   return (
-    <>
-      cli from <span>create-atom-project</span>
-    </>
+    <div className={styles.box}>
+      <div className={styles.state}>
+        <span>count: {count}</span>
+        <button className={styles.btn} onClick={handleClick}>Click</button>
+      </div>
+
+      <div className={styles.words}>
+        <p>A template package for building React application by own cli tool.</p>
+        <p className={styles.author}> --- Lesenelir</p>
+      </div>
+    </div>
   )
 }
 
